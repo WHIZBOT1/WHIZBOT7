faebbd99f0409e6e7acbb2f5fbd5207a30dc5af5
WHIZBOT-Md <167946140+WHIZBOT1@users.noreply.github.com>
2024-04-27 14:37:26 -0500

    Update index.ts

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -2,7 +2,6 @@ import Baileys, { GroupMetadata, ParticipantAction } from '@whiskeysockets/baile
 
 export * from './Config'
 export * from './Command'
-export * from './Pokemon'
 export * from './Message'
 
 export interface IContact {
@@ -54,7 +53,6 @@ export interface YT_Search {
 
 export enum GroupFeatures {
     'events' = 'By enabling this feature, the bot will welcome new members, gives farewell to the members who left the group or were removed and reacts when a member is promoted or demoted',
-    'wild' = 'By enabling this feature, it will send wild pokemon',
     'chara' = '🍁By enabling this feature, the commands for Chara will start working in your group🍁',
     'mods' = 'By enabling this feature, it enables the bot to remove the member (except for admins) which sent an invite link of other groups. This will work if and only if the bots an admin'
  }
